7fb2effc2a35b1de456d0e37c48fe1d38baa559c
Kenneth Ossai <kenossai1@gmail.com>
2025-05-16 15:46:05 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ServiceController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +20,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/contact-us', [ContactController::class, 'index'])->name('contact');
+Route::get('/about-us', [AboutController::class, 'index'])->name('about');
+Route::get('/service', [ServiceController::class, 'index'])->name('service');
