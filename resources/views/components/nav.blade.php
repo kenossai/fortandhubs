78d4c6c53006961bcc1066caf752fb53da22c1c7
Kenneth Ossai <kenossai1@gmail.com>
2025-05-16 13:46:52 +0100

    Update nav.blade.php

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -18,88 +18,30 @@
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="home-design-studio.html#" role="button"
                         aria-haspopup="true" aria-expanded="false"><span
-                            class="rolling-text">Demos</span></a>
-                    <div class="dropdown-menu mega-menu">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-lg">
-                                    <a class="item-img text-center" href="home-creative-agency.html">
-                                        <span class="img"><img src="assets/imgs/menu/1.jpg"
-                                                alt=""></span>
-                                        <span class="mt-15 sub-font fz-18">Creative Agency</span>
-                                    </a>
-                                </div>
-                                <div class="col-lg">
-                                    <a class="item-img text-center" href="home-design-studio.html">
-                                        <span class="img"><img src="assets/imgs/menu/2.jpg"
-                                                alt=""></span>
-                                        <span class="mt-15 sub-font fz-18">Design Studio</span>
-                                    </a>
-                                </div>
-                                <div class="col-lg">
-                                    <a class="item-img text-center" href="home-business-agency.html">
-                                        <span class="img"><img src="assets/imgs/menu/3.jpg"
-                                                alt=""></span>
-                                        <span class="mt-15 sub-font fz-18">Business Agency</span>
-                                    </a>
-                                </div>
-                                <div class="col-lg">
-                                    <a class="item-img text-center" href="home-digital-marketing.html">
-                                        <span class="img"><img src="assets/imgs/menu/4.jpg"
-                                                alt=""></span>
-                                        <span class="mt-15 sub-font fz-18">Digital Marketing</span>
-                                    </a>
-                                </div>
-                                <div class="col-lg">
-                                    <a class="item-img text-center" href="home-digital-agency.html">
-                                        <span class="img"><img src="assets/imgs/menu/5.jpg"
-                                                alt=""></span>
-                                        <span class="mt-15 sub-font fz-18">Digital Agency</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                            class="rolling-text">Home</span></a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="about" role="button"
+                        aria-haspopup="true" aria-expanded="false"><span
+                            class="rolling-text">Pages</span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="home-design-studio.html#" role="button"
                         aria-haspopup="true" aria-expanded="false"><span
-                            class="rolling-text">Pages</span></a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="page-about.html">About Us</a></li>
-                        <li><a class="dropdown-item" href="page-services.html">Services</a></li>
-                        <li><a class="dropdown-item" href="page-team.html">Our Team</a></li>
-                        <li><a class="dropdown-item" href="page-pricing.html">Pricing</a></li>
-                        <li><a class="dropdown-item" href="page-case-study.html">Case Study</a></li>
-                        <li><a class="dropdown-item" href="page-case-details.html">Case Details</a></li>
-                        <li><a class="dropdown-item" href="page-contact.html">Contact Us</a></li>
-                    </ul>
+                            class="rolling-text">Our Service</span></a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="home-design-studio.html#" role="button"
                         aria-haspopup="true" aria-expanded="false"><span
                             class="rolling-text">Projects</span></a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="portfolio-carousel.html">Portfolio Carousel</a>
-                        <a class="dropdown-item" href="portfolio-creative.html">Portfolio Creative</a>
-                        <a class="dropdown-item" href="portfolio-grid.html">Portfolio Grid</a>
-                        <a class="dropdown-item" href="portfolio-tabs.html">Portfolio Tabs</a>
-                        <a class="dropdown-item" href="portfolio-details1.html">Project Details 1</a>
-                        <a class="dropdown-item" href="portfolio-details2.html">Project Details 2</a>
-                    </div>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="home-design-studio.html#" role="button"
                         aria-haspopup="true" aria-expanded="false"><span
                             class="rolling-text">Blogs</span></a>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="page-blogs.html">Blog Standerd</a>
-                        <a class="dropdown-item" href="page-blogs-sidebar.html">Blog Sidebar</a>
-                        <a class="dropdown-item" href="page-blog-details.html">Blog Details</a>
-                    </div>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="page-contact.html">
+                    <a class="nav-link" href="{{ route('contact') }}" >
                         <span class="rolling-text">Contact Us</span>
                     </a>
                 </li>
