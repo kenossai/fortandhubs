92b8d4cefebdc8f3b72879598d59089096c2ff3a
Kenneth Ossai <kenossai1@gmail.com>
2025-06-06 22:40:49 +0100

    created copyright component

---
+++ resources/views/components/copyright.blade.php
@@ -0,0 +1,17 @@
+<div class="rts-copyright-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="text-center">
+                    <p>Invena - Copyright
+                        <script>
+                            document.write(
+                                new Date().getFullYear()
+                            )
+                        </script>. All rights reserved.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
