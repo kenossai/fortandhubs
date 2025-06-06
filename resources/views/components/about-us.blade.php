5fd3b29fbce622c9e2e1b268b14ec7593d6ecea5
Kenneth Ossai <kenossai1@gmail.com>
2025-06-06 23:42:29 +0100

    created about-us component

---
+++ resources/views/components/about-us.blade.php
@@ -0,0 +1,70 @@
+<div class="rts-about-area-two rts-section-gap">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-lg-6">
+                <div class="left-thumbnail-about-area-two">
+                    <img loading="lazy" src="assets/images/about/04.webp" alt="about">
+                    <div class="small-image">
+                        <img loading="lazy" src="assets/images/about/05.webp" alt="small">
+                    </div>
+                    <div class="counter-about-area">
+                        <h2 class="counter title"><span class="odometer" data-count="25">00</span>+
+                        </h2>
+                        <span>Year of experience</span>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6 mt_sm--80 mt_md--80">
+                <div class="about-inner-content-two">
+                    <div class="title-style-two left">
+                        <span class="bg-content">About Us</span>
+                        <span class="pre">More About Us</span>
+                        <h2 class="title rts-text-anime-style-1">Secure Your Future, <br> Transform with Confidence
+                        </h2>
+                    </div>
+                    <div class="about-between-wrapper">
+                        <p class="disc">
+                            At Fort and Hub Global, we empower organizations to thrive in a rapidly evolving digital landscape by delivering
+                            world-class cybersecurity and digital transformation solutions. Our mission is to protect your critical assets, enhance
+                            resilience, and drive innovations securely and sustainably.
+                        </p>
+                        <div class="check-wrapper-area">
+                            <div class="single-check">
+                                <i class="fa-solid fa-circle-check"></i>
+                                <p>24/7 Call Services Avilable</p>
+                            </div>
+                            <div class="single-check">
+                                <i class="fa-solid fa-circle-check"></i>
+                                <p>Skilled Project Experts</p>
+                            </div>
+                            <div class="single-check">
+                                <i class="fa-solid fa-circle-check"></i>
+                                <p>Excellent Team Members</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="call-and-sign-area two">
+                        <div class="call-area">
+                            <div class="icon">
+                                <i class="fa-sharp fa-regular fa-phone-volume"></i>
+                            </div>
+                            <div class="information">
+                                <span>Call us anytime</span>
+                                <a href="index.html#">
+                                    <h6 class="title">+256 56778.5678</h6>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="sign-area">
+                            <img loading="lazy" src="assets/images/about/sign.svg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="shape-area">
+        <img loading="lazy" src="assets/images/about/shape/01.svg" alt="shape" class="one">
+        <img loading="lazy" src="assets/images/about/shape/02.svg" alt="shape" class="two">
+    </div>
+</div>
