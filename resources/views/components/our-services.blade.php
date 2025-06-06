4ed65bd43cfed28563d3f1ce0176b3a709830e9a
Kenneth Ossai <kenossai1@gmail.com>
2025-06-06 23:42:08 +0100

    created service section component

---
+++ resources/views/components/our-services.blade.php
@@ -0,0 +1,103 @@
+<div class="rts-service-area pt--40 pb--60">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="title-style-two center">
+                    <span class="bg-content">Services</span>
+                    <span class="pre">Service</span>
+                    <h2 class="title rts-text-anime-style-1">Our Competences
+                    </h2>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container-2 mt--30">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="service-bg-style-one-wrapper">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="service-style-swiper-wrapper-two">
+                                <div class="swiper mySwiper-service-1">
+                                    <div class="swiper-wrapper">
+                                        <div class="swiper-slide">
+                                            <div class="single-service-signle-wrapper">
+                                                <div class="icons">
+                                                    <img loading="lazy" src="{{ asset('assets/images/service/icons/01.svg') }}" alt="service">
+                                                </div>
+                                                <div class="information">
+                                                    <h5 class="title">Media Solution &amp; Services:</h5>
+                                                    <p class="disc">
+                                                        Our company offers a comprehensive media solution that covers brand management, website development, and campaign management..
+                                                    </p>
+                                                    <a href="service-details.html" class="arrow-right">
+                                                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                                                        <span>Read More</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="single-service-signle-wrapper">
+                                                <div class="icons">
+                                                    <img loading="lazy" src="assets/images/service/icons/02.svg" alt="service">
+                                                </div>
+                                                <div class="information">
+                                                    <h5 class="title">Digital Transformation Services</h5>
+                                                    <p class="disc">
+                                                        We provide comprehensive digital transformation solution that covers various areas, including Customer Relationship Management (CRM), Business Automation...
+                                                    </p>
+                                                    <a href="service-details.html" class="arrow-right">
+                                                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                                                        <span>Read More</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="single-service-signle-wrapper">
+                                                <div class="icons">
+                                                    <img loading="lazy" src="assets/images/service/icons/03.svg" alt="service">
+                                                </div>
+                                                <div class="information">
+                                                    <h5 class="title">Strategy & Planning</h5>
+                                                    <p class="disc">
+                                                        Sagitis himos pulvinar morb socis laoreet posuere enim non
+                                                        auctor etiam
+                                                        pretium libero
+                                                    </p>
+                                                    <a href="service-details.html" class="arrow-right">
+                                                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                                                        <span>Read More</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="swiper-slide">
+                                            <div class="single-service-signle-wrapper">
+                                                <div class="icons">
+                                                    <img loading="lazy" src="assets/images/service/icons/01.svg" alt="service">
+                                                </div>
+                                                <div class="information">
+                                                    <h5 class="title">Cybersecurity Service</h5>
+                                                    <p class="disc">
+                                                        We offer end-to-end cybersecurity and digital transformation solutions designed to address your unique needs.
+                                                    </p>
+                                                    <a href="index.html#" class="arrow-right">
+                                                        <i class="fa-sharp fa-solid fa-arrow-right"></i>
+                                                        <span>Read More</span>
+                                                    </a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="swiper-pagination"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
