8b144dc8484c39dce0a90ea8e10abb88f18755e2
Kenneth Ossai <kenossai1@gmail.com>
2025-06-06 22:41:24 +0100

    updated loader component

--- resources/views/components/loader.blade.php
+++ resources/views/components/loader.blade.php
@@ -1,17 +1,6 @@
-<div class="loader-wrap">
-    <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
-        <path id="svg" d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"></path>
-    </svg>
-
-    <div class="loader-wrap-heading">
-        <div class="load-text">
-            <span>L</span>
-            <span>o</span>
-            <span>a</span>
-            <span>d</span>
-            <span>i</span>
-            <span>n</span>
-            <span>g</span>
-        </div>
+<div class="loader-wrapper">
+    <div class="loader">
     </div>
+    <div class="loader-section section-left"></div>
+    <div class="loader-section section-right"></div>
 </div>
