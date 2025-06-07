84a46a207316dc964ad326fbbe6c50b54ec9ddc2
Kenneth Ossai <kenossai1@gmail.com>
2025-06-07 20:29:38 +0100

    Updated subscription email

--- resources/views/emails/subscription_confirmation.blade.php
+++ resources/views/emails/subscription_confirmation.blade.php
@@ -1,5 +1,52 @@
-<h2>Thank you for subscribing!</h2>
-<p>We're happy to have you join us</p>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
 
-<p>If you ever change your mind, you can unsubscribe by clicking below:</p>
-<a href="{{ route('unsubscribe', $subscriber->unsubscribe_token) }}">Unsubscribe</a>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Thank you for subscribing</title>
+  <!--[if mso]><style type="text/css">body, table, td, a { font-family: Arial, Helvetica, sans-serif !important; }</style><![endif]-->
+</head>
+
+<body style="font-family: Helvetica, Arial, sans-serif; margin: 0px; padding: 0px; background-color: #ffffff;">
+  <table role="presentation"
+    style="width: 100%; border-collapse: collapse; border: 0px; border-spacing: 0px; font-family: Arial, Helvetica, sans-serif; background-color: rgb(239, 239, 239);">
+    <tbody>
+      <tr>
+        <td align="center" style="padding: 1rem 2rem; vertical-align: top; width: 100%;">
+          <table role="presentation" style="max-width: 600px; border-collapse: collapse; border: 0px; border-spacing: 0px; text-align: left;">
+            <tbody>
+              <tr>
+                <td style="padding: 40px 0px 0px;">
+                  <div style="text-align: left;">
+                    <div style="padding-bottom: 20px;"><img
+                        src="{{ asset('assets/images/logo/logo.png') }}" alt="Company"
+                        style="width: 100px;"></div>
+                  </div>
+                  <div style="padding: 20px; background-color: rgb(255, 255, 255);">
+                    <div style="color: rgb(0, 0, 0); text-align: left;">
+                      <h1 style="margin: 1rem 0">Thanks for subscribing!</h1>
+                      <p style="padding-bottom: 16px">The FortanHubs Newsletter is the best way to find out about the universe <em>...and
+                          beyond</em>.</p>
+                      <p style="padding-bottom: 16px">Once or twice a month, you will now receive our newsletter with information about new
+                        world, planets, solar systems, as well as science, new discoveries and much more. </p>
+                      <p style="padding-bottom: 16px">Of course, you can <a href="{{ route('unsubscribe', $subscriber->unsubscribe_token) }}" target="_blank" style="text-decoration: none;">Unsubscribe</a>.</p>
+                      <p style="padding-bottom: 16px">We're really happy to have you on board!</p>
+                      <p style="padding-bottom: 16px">Best regards,<br><em>The Mailmeteor team</em></p>
+                    </div>
+                  </div>
+                  <div style="padding-top: 20px; color: rgb(153, 153, 153); text-align: center;">
+                    <p style="padding-bottom: 16px">Made with ♥ in Paris</p>
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+
+</html>
+
