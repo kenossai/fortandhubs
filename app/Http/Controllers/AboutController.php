53c50319eed7c7fabd3b903327d4b2001ee58288
Kenneth Ossai <kenossai1@gmail.com>
2025-06-14 22:50:56 +0100

    Update AboutController.php

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -8,6 +8,7 @@ class AboutController extends Controller
 {
     public function index()
     {
-        return view('pages.about');
+        $about = \App\Models\About::first();
+        return view('pages.about', compact('about'));
     }
 }
