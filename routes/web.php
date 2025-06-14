d00b6bf1c25f91199679169488fb5892bb7bc18e
Kenneth Ossai <kenossai1@gmail.com>
2025-06-14 22:52:43 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AboutController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ServiceController;
 use App\Http\Controllers\SubscriptionController;
 
@@ -21,6 +22,7 @@ use App\Http\Controllers\SubscriptionController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::get('/contact-us', [ContactController::class, 'index'])->name('contact');
+Route::post('/contact/send', [MessageController::class, 'store'])->name('messages.store');
 
 Route::get('/about-us', [AboutController::class, 'index'])->name('about');
 
