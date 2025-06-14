2efb421af4b516c72b4397cc2cd8ffd941d9cc46
Kenneth Ossai <kenossai1@gmail.com>
2025-06-14 18:09:25 +0100

    Create CreateHomepageAbout.php

---
+++ app/Filament/Resources/HomepageAboutResource/Pages/CreateHomepageAbout.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\HomepageAboutResource\Pages;
+
+use App\Filament\Resources\HomepageAboutResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateHomepageAbout extends CreateRecord
+{
+    protected static string $resource = HomepageAboutResource::class;
+}
