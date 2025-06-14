4021c9eedc2e2fb98c38955b250db8ae1c0c0581
Kenneth Ossai <kenossai1@gmail.com>
2025-06-14 22:52:36 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <!-- ==================== Start Intro ==================== -->
 @section('title', 'Home Page')
-<x-banner :slides="$slides" />
+<x-banner :slides="$slides" :clients="$clients" />
 
     <!-- rts about area start -->
     <x-about-us />
@@ -255,12 +255,9 @@
             </div>
             <div class="row">
                 <div class="client-wrapper-one">
-                    <a href="index.html#" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2"><img src="assets/images/client/1.webp" alt="business_invena"></a>
-                    <a href="index.html#" data-animation="fadeInUp" data-delay="0.2" data-duration="1.2"><img src="assets/images/client/2.webp" alt="business_invena"></a>
-                    <a href="index.html#" data-animation="fadeInUp" data-delay="0.3" data-duration="1.2"><img src="assets/images/client/3.webp" alt="business_invena"></a>
-                    <a href="index.html#" data-animation="fadeInUp" data-delay="0.4" data-duration="1.2"><img src="assets/images/client/4.webp" alt="business_invena"></a>
-                    <a href="index.html#" data-animation="fadeInUp" data-delay="0.5" data-duration="1.2"><img src="assets/images/client/5.webp" alt="business_invena"></a>
-                    <a href="index.html#" data-animation="fadeInUp" data-delay="0.6" data-duration="1.2"><img src="assets/images/client/6.webp" alt="business_invena"></a>
+                    @foreach ($clients as $client)
+                        <a href="index.html#" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2"><img src="{{ asset('storage/' . $client->image_path) }}" alt="fortandhubs"></a>
+                    @endforeach
                 </div>
             </div>
         </div>
@@ -557,111 +554,6 @@
 
 
 
-    <!-- rts blog area start -->
-    <div class="rts-blog-area rts-section-gapBottom pt--40">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="title-style-two center">
-                        <span class="bg-content">Blog</span>
-                        <span class="pre">Blog & News</span>
-                        <h2 class="title">Recent blog post
-                        </h2>
-                    </div>
-                </div>
-            </div>
-            <div class="row g-5 mt--20">
-                <div class="col-lg-12">
-                    <div class="blog-swiper-style-one">
-                        <div class="swiper mySwiper-blog-one">
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide">
-                                    <div class="single-blog-area-one">
-                                        <p>Business Solution / <span>by David Dolean</span></p>
-                                        <a href="blog-details.html">
-                                            <h4 class="title">Building a Brand That from Market Leaders</h4>
-                                        </a>
-                                        <div class="bottom-details">
-                                            <a href="blog-details.html" class="thumbnail">
-                                                <img loading="lazy" src="assets/images/blog/01.webp" alt="blog-area">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="single-blog-area-one">
-                                        <p>Business Solution / <span>by David Dolean</span></p>
-                                        <a href="blog-details.html">
-                                            <h4 class="title">Transforming Your Best Of Approach to Business</h4>
-                                        </a>
-                                        <div class="bottom-details">
-                                            <a href="blog-details.html" class="thumbnail">
-                                                <img loading="lazy" src="assets/images/blog/02.webp" alt="blog-area">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="single-blog-area-one">
-                                        <p>Business Solution / <span>by David Dolean</span></p>
-                                        <a href="blog-details.html">
-                                            <h4 class="title">The quick settle tips of the new ages exist</h4>
-                                        </a>
-                                        <div class="bottom-details">
-                                            <a href="blog-details.html" class="thumbnail">
-                                                <img loading="lazy" src="assets/images/blog/03.webp" alt="blog-area">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="single-blog-area-one">
-                                        <p>Business Solution / <span>by David Dolean</span></p>
-                                        <a href="blog-details.html">
-                                            <h4 class="title">Building a Brand That from Market Leaders</h4>
-                                        </a>
-                                        <div class="bottom-details">
-                                            <a href="blog-details.html" class="thumbnail">
-                                                <img loading="lazy" src="assets/images/blog/01.webp" alt="blog-area">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="single-blog-area-one">
-                                        <p>Business Solution / <span>by David Dolean</span></p>
-                                        <a href="blog-details.html">
-                                            <h4 class="title">Transforming Your Best Of Approach to Business</h4>
-                                        </a>
-                                        <div class="bottom-details">
-                                            <a href="blog-details.html" class="thumbnail">
-                                                <img loading="lazy" src="assets/images/blog/02.webp" alt="blog-area">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="single-blog-area-one">
-                                        <p>Business Solution / <span>by David Dolean</span></p>
-                                        <a href="blog-details.html">
-                                            <h4 class="title">The quick settle tips of the new ages exist</h4>
-                                        </a>
-                                        <div class="bottom-details">
-                                            <a href="blog-details.html" class="thumbnail">
-                                                <img loading="lazy" src="assets/images/blog/03.webp" alt="blog-area">
-                                            </a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="swiper-pagination"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- rts blog area end -->
 
 
 </x-app-layout>
