74225ac679f43be7e7ba6219a3f7e1613a6411cc
Kenneth Ossai <kenossai1@gmail.com>
2025-06-14 22:50:38 +0100

    Create TeamMemberResource.php

---
+++ app/Filament/Resources/TeamMemberResource.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\TeamMember;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Forms\Components\FileUpload;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\TeamMemberResource\Pages;
+use App\Filament\Resources\TeamMemberResource\RelationManagers;
+
+class TeamMemberResource extends Resource
+{
+    protected static ?string $model = TeamMember::class;
+
+    protected static ?string $navigationGroup = 'Pages > Homepage';
+    protected static ?int $navigationSort = 5;
+    protected static ?string $navigationLabel = 'Team Members';
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form->schema([
+            TextInput::make('name')->required(),
+            TextInput::make('role')->required(),
+            FileUpload::make('photo')
+                ->image()
+                ->directory('team')
+                ->required(),
+        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table->columns([
+            ImageColumn::make('photo')->disk('public')->width(50),
+            TextColumn::make('name')->searchable(),
+            TextColumn::make('role'),
+        ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTeamMembers::route('/'),
+            'create' => Pages\CreateTeamMember::route('/create'),
+            'edit' => Pages\EditTeamMember::route('/{record}/edit'),
+        ];
+    }
+}
