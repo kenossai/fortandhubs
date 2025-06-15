9624259557e1768c1812532b84561480ffb81b9a
Kenneth Ossai <kenossai1@gmail.com>
2025-06-15 19:22:29 +0100

    Update about.blade.php

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -8,9 +8,9 @@
                 <div class="col-lg-12">
                     <div class="title-area-left">
                         <span class="pre">About Us</span>
-                        <span class="bg-title">About Us</span>
+                        <span class="bg-title">Fort & Hubs</span>
                         <h1 class="title rts-text-anime-style-1">
-                            {!! $about->title !!}
+                            {!! str_replace('<br>', $about->title) !!}
                         </h1>
                     </div>
                 </div>
@@ -97,152 +97,7 @@
 
 
     <!-- rts team area start -->
-    <div class="rts-team-area rts-section-gapBottom pt--40">
-        <div class="container">
-            <div class="row">
-                <div class="title-style-two center">
-                    <span class="bg-content">Members</span>
-                    <span class="pre">Expert Members</span>
-                    <h2 class="title rts-text-anime-style-1">Professionals Team
-                    </h2>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="swiper mySwiperh1_team pt--50 pb--80">
-                        <div class="swiper-wrapper">
-                            <div class="swiper-slide">
-                                <div class="team-single-one-start">
-                                    <div class="team-image-area">
-                                        <a href="team-details.html">
-                                            <img src="assets/images/team/04.webp" alt="Business_Team_single">
-                                            <div class="team-social">
-                                                <div class="main">
-                                                    <i class="fal fa-plus"></i>
-                                                </div>
-                                                <div class="team-social-one">
-                                                    <i class="fab fa-youtube"></i>
-                                                    <i class="fab fa-twitter"></i>
-                                                    <i class="fab fa-instagram"></i>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="single-details">
-                                        <a href="team-details.html">
-                                            <h5 class="title">Archer Graham</h5>
-                                        </a>
-                                        <p>Founder</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="team-single-one-start">
-                                    <div class="team-image-area">
-                                        <a href="team-details.html">
-                                            <img src="assets/images/team/05.webp" alt="Business_Team_single">
-                                            <div class="team-social">
-                                                <div class="main">
-                                                    <i class="fal fa-plus"></i>
-                                                </div>
-                                                <div class="team-social-one">
-                                                    <i class="fab fa-youtube"></i>
-                                                    <i class="fab fa-twitter"></i>
-                                                    <i class="fab fa-instagram"></i>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="single-details">
-                                        <a href="team-details.html">
-                                            <h5 class="title">Amelia Clover</h5>
-                                        </a>
-                                        <p>Co-Founder</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="team-single-one-start">
-                                    <div class="team-image-area">
-                                        <a href="team-details.html">
-                                            <img src="assets/images/team/06.webp" alt="Business_Team_single">
-                                            <div class="team-social">
-                                                <div class="main">
-                                                    <i class="fal fa-plus"></i>
-                                                </div>
-                                                <div class="team-social-one">
-                                                    <i class="fab fa-youtube"></i>
-                                                    <i class="fab fa-twitter"></i>
-                                                    <i class="fab fa-instagram"></i>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="single-details">
-                                        <a href="team-details.html">
-                                            <h5 class="title">Beckett Hayden</h5>
-                                        </a>
-                                        <p>Deputy Manager</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="team-single-one-start">
-                                    <div class="team-image-area">
-                                        <a href="team-details.html">
-                                            <img src="assets/images/team/07.webp" alt="Business_Team_single">
-                                            <div class="team-social">
-                                                <div class="main">
-                                                    <i class="fal fa-plus"></i>
-                                                </div>
-                                                <div class="team-social-one">
-                                                    <i class="fab fa-youtube"></i>
-                                                    <i class="fab fa-twitter"></i>
-                                                    <i class="fab fa-instagram"></i>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="single-details">
-                                        <a href="team-details.html">
-                                            <h5 class="title">Julian Wyat</h5>
-                                        </a>
-                                        <p>Finance Manager</p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="swiper-slide">
-                                <div class="team-single-one-start">
-                                    <div class="team-image-area">
-                                        <a href="team-details.html">
-                                            <img src="assets/images/team/07.webp" alt="Business_Team_single">
-                                            <div class="team-social">
-                                                <div class="main">
-                                                    <i class="fal fa-plus"></i>
-                                                </div>
-                                                <div class="team-social-one">
-                                                    <i class="fab fa-youtube"></i>
-                                                    <i class="fab fa-twitter"></i>
-                                                    <i class="fab fa-instagram"></i>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    </div>
-                                    <div class="single-details">
-                                        <a href="team-details.html">
-                                            <h5 class="title">Julian Wyat</h5>
-                                        </a>
-                                        <p>Finance Manager</p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="swiper-pagination"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-team-member :teamMembers="$teamMembers" />
     <!-- rts team area end -->
 
 
@@ -418,24 +273,9 @@
                             <p>Our Trusted Clients</p>
                         </div>
                         <div class="right-brand-area-wrapper">
-                            <div class="single-image">
-                                <img src="assets/images/brand/01.webp" alt="brand">
-                            </div>
-                            <div class="single-image">
-                                <img src="assets/images/brand/02.webp" alt="brand">
-                            </div>
-                            <div class="single-image">
-                                <img src="assets/images/brand/03.webp" alt="brand">
-                            </div>
-                            <div class="single-image">
-                                <img src="assets/images/brand/04.webp" alt="brand">
-                            </div>
-                            <div class="single-image">
-                                <img src="assets/images/brand/05.webp" alt="brand">
-                            </div>
-                            <div class="single-image">
-                                <img src="assets/images/brand/06.webp" alt="brand">
-                            </div>
+                            @foreach ($clients as $client)
+                                <a href="index.html#" data-animation="fadeInUp" data-delay="0.1" data-duration="1.2"><img src="{{ asset('storage/' . $client->image_path) }}" alt="fortandhubs"></a>
+                            @endforeach
                         </div>
                     </div>
                 </div>
