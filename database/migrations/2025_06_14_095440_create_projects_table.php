a6ddef32726a155232997661cbd328b4b0e23344
Kenneth Ossai <kenossai1@gmail.com>
2025-06-15 20:45:01 +0100

    Update 2025_06_14_095440_create_projects_table.php

--- database/migrations/2025_06_14_095440_create_projects_table.php
+++ database/migrations/2025_06_14_095440_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('title');
             $table->text('description')->nullable();
-            $table->string('image')->nullable();
+            $table->string('image_path')->nullable();
             $table->string('link')->nullable();
             $table->timestamps();
         });
