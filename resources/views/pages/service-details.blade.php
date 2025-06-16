6b82a3564e03cf065f6bb4dd4ebd2a1dd71115ec
Kenneth Ossai <kenossai1@gmail.com>
2025-06-16 13:06:03 +0100

    Create service-details.blade.php

---
+++ resources/views/pages/service-details.blade.php
@@ -0,0 +1,155 @@
+<x-app-layout>
+
+    @section('title', 'Services Details')
+
+    <div class="breadcrumb-service-detals-one">
+        <div class="container-full">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner-inner-service-details-1 large-height bg_image">
+                        <div class="container">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <div class="title-area-left">
+                                        <span class="bg-title ">Business</span>
+                                        <h1 class="title rts-text-anime-style-1">
+                                            Business Solution
+                                        </h1>
+                                        <p class="disc">
+                                            Intrinsicly coordinate multifunctional functionalities reliable
+                                            potentialities.
+                                            Objectively envisioneer high in convergence through collaborative networks.
+                                            Interactively generate B2C e-tailers for business data restore fully
+                                            researched
+                                            relationships through resource maximizing results.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- rts business details area left main -->
+    <div class="rts-service-details-area-main-bottom ">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 offset-lg-2">
+                    <div class="service-details-left-area mt--0 rts-section-gapTop">
+                        <h3 class="title rts-text-anime-style-1">Business Growth Mangment</h3>
+                        <p class="disc">
+                            Continually myocardinate holistic mindshare with client-based web services. Assertively
+                            e-enable catalysts for change before tested markets. Phosfluorescently maintain wireless
+                            scenarios after intermandated applications. Conveniently predominate revolutionary quality
+                            vectors through future-proof manufactured products. Enthusiastically transform distinctive
+                            collaboration.
+                        </p>
+                        <p class="disc">
+                            Intrinsicly coordinate multifunctional functionalities reliable potentialities. Objectively
+                            envisioneer high in convergence through collaborative networks. Interactively generate B2C
+                            e-tailers for business data restore fully researched relationships through resource
+                            maximizing results.
+                        </p>
+                        <div class="service-short-main-wrapper">
+                            <div class="single-short-service">
+                                <div class="icon">
+                                    <img src="assets/images/service/05.svg" alt="">
+                                </div>
+                                <div class="inner-content">
+                                    <h5 class="title-sm">Instant Business Growth</h5>
+                                    <p class="disc">
+                                        Maintain wireless scerios after sure quality vectors future
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="single-short-service">
+                                <div class="icon">
+                                    <img src="assets/images/service/06.svg" alt="">
+                                </div>
+                                <div class="inner-content">
+                                    <h5 class="title-sm">24/7 Quality Service</h5>
+                                    <p class="disc">
+                                        Maintain wireless scerios after sure quality vectors future
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="single-short-service">
+                                <div class="icon">
+                                    <img src="assets/images/service/07.svg" alt="">
+                                </div>
+                                <div class="inner-content">
+                                    <h5 class="title-sm">Easy Customer Service</h5>
+                                    <p class="disc">
+                                        Maintain wireless scerios after sure quality vectors future
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="single-short-service">
+                                <div class="icon">
+                                    <img src="assets/images/service/08.svg" alt="">
+                                </div>
+                                <div class="inner-content">
+                                    <h5 class="title-sm">Quality Cost Service</h5>
+                                    <p class="disc">
+                                        Maintain wireless scerios after sure quality vectors future
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <p class="disc mt--30">
+                            Phosfluorescently maintain wireless scenarios after intermandated applications. Conveniently
+                            unique predominate revolutionary quality vectors through future-proof manufactured products.
+                            Objectively envisioneer high solution convergence through collaborative networks.
+                            Interactively generate B2C e-tailers for business data restore fully researched
+                            relationships through resource maximizing results.
+                        </p>
+                        <h3 class="title mt--50 rts-text-anime-style-1">2 Simple Steps to Process</h3>
+                        <p class="disc">
+                            Assertively e-enable catalysts for change before fully tested markets. Phosfluo rescently is
+                            maintain solve wireless scenarios after intermandated applications. Conveniently predominate
+                            busin revolutionary quality vectors through future-proof manufactured products.
+                            Enthusiastically transform distinctive collaboration.
+                        </p>
+                        <p class="disc">
+                            Phosfluorescently maintain wireless scenarios after intermandated applications. Conveniently
+                            predominate misslat revolutionary quality vectors through future-proof manufactured
+                            products.
+                        </p>
+                        <div class="single-step-service-details">
+                            <div class="thumbnail">
+                                <img src="assets/images/service/12.webp" alt="service">
+                            </div>
+                            <div class="inner">
+                                <h5 class="title-f">Define Your Objectives and Plan</h5>
+                            </div>
+                            <div class="feature">
+                                <p class="single">Outline Key Features</p>
+                                <p class="single">Choose a Domain Name</p>
+                                <p class="single">Identify Purpose</p>
+                                <p class="single">Understand Your Audience</p>
+                            </div>
+                        </div>
+                        <div class="single-step-service-details">
+                            <div class="thumbnail">
+                                <img src="assets/images/service/13.webp" alt="service">
+                            </div>
+                            <div class="inner">
+                                <h5 class="title-f">Build and Design Your Website</h5>
+                            </div>
+                            <div class="feature">
+                                <p class="single">Outline Key Features</p>
+                                <p class="single">Choose a Domain Name</p>
+                                <p class="single">Identify Purpose</p>
+                                <p class="single">Understand Your Audience</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- rts business details area left main end -->
+</x-app-layout>
