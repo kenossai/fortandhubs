f59b4c996f381510ee4e7af30c525866778d89ab
Kenneth Ossai <kenossai1@gmail.com>
2025-06-16 13:05:56 +0100

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -172,75 +172,7 @@
     <!-- rts counter up area end -->
 
     <!-- start gallery section -->
-    <div class="rts-gallery-area rts-section-gap gallery-bg bg_image">
-        <div class="container pt--40">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="title-area-between-wrapper-gallery-project">
-                        <div class="title-style-two mb--40 left">
-                            <span class="bg-content">Projects</span>
-                            <span class="pre">Popular Projects</span>
-                            <h2 class="title rts-text-anime-style-1">Projects
-                                Our Completed Projects
-                            </h2>
-                        </div>
-
-                        <div class="swiper-paginations"></div>
-                    </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="project-style-one-wrapper">
-
-                        <div class="swiper mySwiper-project-1">
-                            <div class="swiper-wrapper">
-                                <div class="swiper-slide">
-                                    <div class="project-style-one">
-                                        <a href="project-details.html" class="thumbnail">
-                                            <img loading="lazy" src="assets/images/project/01.webp" alt="project">
-                                        </a>
-                                        <div class="inner-content">
-                                            <a href="project-details.html">
-                                                <h5 class="title">Business Growth</h5>
-                                            </a>
-                                            <span>Business Strategy</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="project-style-one">
-                                        <a href="project-details.html" class="thumbnail">
-                                            <img loading="lazy" src="assets/images/project/02.webp" alt="project">
-                                        </a>
-                                        <div class="inner-content">
-                                            <a href="project-details.html">
-                                                <h5 class="title">Startup Solution</h5>
-                                            </a>
-                                            <span>Business Strategy</span>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="project-style-one">
-                                        <a href="project-details.html" class="thumbnail">
-                                            <img loading="lazy" src="assets/images/project/01.webp" alt="project">
-                                        </a>
-                                        <div class="inner-content">
-                                            <a href="project-details.html">
-                                                <h5 class="title">Marketing Growth</h5>
-                                            </a>
-                                            <span>Business Strategy</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-home-projects :projects="$projects" />
     <!-- start gallery section -->
 
 
