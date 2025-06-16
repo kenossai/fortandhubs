7991b90dac0588903950cb936aa3bae76561be22
Kenneth Ossai <kenossai1@gmail.com>
2025-06-16 13:05:45 +0100

    Update 2025_06_14_095440_create_projects_table.php

--- database/migrations/2025_06_14_095440_create_projects_table.php
+++ database/migrations/2025_06_14_095440_create_projects_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use PHPUnit\Event\Telemetry\Duration;
 
 return new class extends Migration
 {
@@ -14,8 +15,18 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->text('description')->nullable();
-            $table->string('image_path')->nullable();
+            $table->text('sub_title')->nullable();
+            $table->longText('description')->nullable();
+            $table->text('image_path')->nullable();
+            $table->string('slug')->unique();
+            $table->string('client_name')->nullable();
+            $table->string('category')->nullable();
+            $table->string('status')->default('active'); // active, inactive, archived
+            $table->date('project_year')->nullable();
+            $table->string('location')->nullable();
+            $table->string('price')->nullable();
+            $table->text('project_approach')->nullable(); // e.g., ['Design', 'Development', 'Testing']
+            $table->string('duration')->nullable(); // e.g., '3 months', '1 year'
             $table->string('link')->nullable();
             $table->timestamps();
         });
