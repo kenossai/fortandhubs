5b0af4039f93f6a2b87cbbd47272174cf9da5cac
Kenneth Ossai <kenossai1@gmail.com>
2025-06-16 13:05:40 +0100

    Update Service.php

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Service extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'icon',
+        'sub_content',
+        'content',
+        'photo',
+    ];
 }
