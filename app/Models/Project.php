63949279ff8d2e406fb8f760e1349ca9474b7b40
Kenneth Ossai <kenossai1@gmail.com>
2025-06-16 13:05:34 +0100

    Update Project.php

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'sub_title',
+        'description',
+        'image_path',
+        'slug',
+        'client_name',
+        'category',
+        'status',
+        'project_year',
+        'location',
+        'duration',
+        'price',
+        'project_approach',
+        'link',
+    ];
+
+    protected $casts = [
+        'image_path' => 'array',
+        'project_approach' => 'array',
+    ];
+
 }
