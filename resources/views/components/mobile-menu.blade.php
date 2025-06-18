7e6d8c4fabe60cfb20cbb11726f55eb37ba321ac
Kenneth Ossai <kenossai1@gmail.com>
2025-06-18 13:55:31 +0100

    Update mobile-menu.blade.php

--- resources/views/components/mobile-menu.blade.php
+++ resources/views/components/mobile-menu.blade.php
@@ -1,172 +1,21 @@
 <div class="mobile-menu d-block d-xl-none">
     <nav class="nav-main mainmenu-nav mt--30">
         <ul class="mainmenu metismenu" id="mobile-menu-active">
-            <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Demos</a>
-                <ul class="submenu mm-collapse" style="height: 0px;">
-                    <li><a href="index.html">Business One</a></li>
-                    <li><a href="index-two.html">Business Two</a></li>
-                    <li><a href="index-three.html">Business Three</a></li>
-                    <li><a href="index-four.html">Business Four</a></li>
-                    <li><a href="index-five.html">Finance Demo</a></li>
-                    <li><a href="index-six.html">Marketing agency</a></li>
-                    <li><a href="index-seven.html">Business agency</a></li>
-                    <li><a href="index-eight.html">Business Management</a></li>
-                    <li><a href="index-nine.html">Insurance Home</a></li>
-                    <li><a href="index-ten.html">Business Website</a></li>
-                    <li><a href="index-eleven.html">Business Parallax</a></li>
-                    <li><a href="index-twelve.html">Business Video</a></li>
-                    <li><a href="index-thirteen.html">Accountent One</a></li>
-                    <li><a href="index-fourteen.html">Accountent Two</a></li>
-                    <li><a href="index-fifteen.html">HR Website</a></li>
-                    <li><a href="index-sixteen.html">Business Coach</a></li>
-                    <li><a href="index-seventeen.html">SEO Website</a></li>
-                </ul>
+            <li class="">
+                <a href="{{ route('home') }}" class="main">Home</a>
             </li>
-            <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Onepage</a>
-                <ul class="submenu mm-collapse" style="height: 0px;">
-                    <li><a href="onepage-one.html">Business One</a></li>
-                    <li><a href="onepage-two.html">Business Two</a></li>
-                    <li><a href="onepage-three.html">Business Three</a></li>
-                    <li><a href="onepage-four.html">Business Four</a></li>
-                    <li><a href="onepage-five.html">Finance Demo</a></li>
-                    <li><a href="onepage-six.html">Marketing agency</a></li>
-                    <li><a href="onepage-seven.html">Business agency</a></li>
-                    <li><a href="onepage-eight.html">Business Management</a></li>
-                    <li><a href="onepage-nine.html">Insurance Home</a></li>
-                    <li><a href="onepage-ten.html">Business Website</a></li>
-                    <li><a href="onepage-eleven.html">Business Parallax</a></li>
-                    <li><a href="onepage-twelve.html">Business Video</a></li>
-                    <li><a href="onepage-thirteen.html">Accountent One</a></li>
-                    <li><a href="onepage-fourteen.html">Accountent Two</a></li>
-                    <li><a href="onepage-fifteen.html">HR Website</a></li>
-                    <li><a href="onepage-sixteen.html">Business Coach</a></li>
-                    <li><a href="onepage-seventeen.html">SEO Website</a></li>
-                </ul>
-            </li>
-            <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Pages</a>
-                <ul class="submenu mm-collapse" style="height: 0px;">
-                    <li><a href="about.html">About
-                            Company</a></li>
-                    <li><a href="service.html">Service</a></li>
-                    <li><a href="service-details.html">Service
-                            Details</a>
-                    </li>
-                    <li><a href="service-details-2.html">Service
-                            Details 2</a>
-                    </li>
-                    <li><a href="project.html">Project</a>
-                    </li>
-                    <li><a href="team.html">Team</a>
-                    </li>
-                    <li><a href="team-details.html">Team
-                            Details</a>
-                    </li>
-                    <li><a href="pricing.html">Pricing</a>
-                    </li>
-                    <li><a href="appoinment.html">Appoinment</a>
-                    </li>
-                    <li><a href="history.html">Our
-                            History</a>
-                    </li>
-                    <li><a href="blog-list.html">Blog
-                            List</a>
-                    </li>
-                    <li><a href="blog-grid.html">Blog
-                            List</a>
-                    </li>
-                    <li><a href="blog-details.html">Blog
-                            Details</a>
-                    </li>
-                    <li><a href="blog-details-2.html">Blog
-                            Details 02</a>
-                    </li>
-                    <li><a href="faq.html">Faq's</a>
-                    <li><a href="career.html">Career</a>
-                    </li>
-                    <li><a href="our-mission.html">Our
-                            Mission</a>
-                    </li>
-                    <li><a href="partners.html">Partners</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Services</a>
-                <ul class="submenu mm-collapse" style="height: 0px;">
-                    <li><a class="mobile-menu-link" href="service.html">Service</a></li>
-                    <li><a class="mobile-menu-link" href="service-details.html">Service Details</a></li>
-                    <li><a class="mobile-menu-link" href="service-details-2.html">Service Details 2</a></li>
-                    <li><a class="mobile-menu-link" href="service-details-3.html">Service Details 3</a></li>
-                    <li><a class="mobile-menu-link" href="service-details-4.html">Service Details 4</a></li>
-                    <li><a class="mobile-menu-link" href="service-details-5.html">Service Details 5</a></li>
+            <li class="">
+                <a href="{{ route('about') }}" class="main">About</a>
 
-                </ul>
+            </li>
+            <li class="">
+                <a href="{{ route('service') }}" class="main">Services</a>
             </li>
             <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Projects</a>
-                <ul class="submenu mm-collapse">
-                    <li><a href="project.html">Project</a>
-                    </li>
-                    <li><a href="project-slider.html">Project
-                            Slider</a></li>
-                    <li><a href="project-slider-2.html">Project
-                            Slider 2</a></li>
-                    <li><a href="project-card-slider.html">Project
-                            Card Slider</a></li>
-                    <li><a href="project-bg-dark.html">Project
-                            Bg Dark</a></li>
-                    <li><a href="project-grid.html">Project
-                            Grid</a></li>
-                    <li><a href="project-grid-col-2.html">Project
-                            Grid Col 2</a></li>
-                    <li><a href="project-slider-grid.html">Grid Slider</a></li>
-                    <li><a href="project-slider-overflow.html">Project Overflow</a>
-                    </li>
-                    <li><a href="project-grid-col-3.html">Project Grid Col 3</a>
-                    </li>
-                    <li><a href="project-bg-dark.html">Project
-                            Hide Content</a></li>
-
-
-                    <li><a href="project-slider-2.html">Project
-                            Hide
-                            Content wide</a>
-                    </li>
-                    <li><a href="project-grid.html">Project
-                            Card Hover</a>
-                    </li>
-                    <li><a href="project-slider-2.html">Project
-                            Slider Image
-                            Zoom</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Shop Pages</a>
-                <ul class="submenu mm-collapse">
-                    <li><a href="shop.html">Shop</a>
-                    </li>
-                    <li><a href="shop-single.html">Shop
-                            Details</a>
-                    </li>
-                    <li><a href="account.html">Account</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="has-droupdown">
-                <a href="index.html#" class="main" aria-expanded="false">Blog</a>
-                <ul class="submenu mm-collapse">
-                    <li><a href="blog-grid.html">Blog Grid</a></li>
-                    <li><a href="blog-list.html">Blog List</a></li>
-                    <li><a href="blog-details.html">Blog Details</a></li>
-                    <li><a href="blog-details-2.html">Blog Details 2</a></li>
-                </ul>
+                <a href="{{ route('projects') }}" class="main" >Projects</a>
             </li>
             <li>
-                <a href="contact.html" class="main" aria-expanded="false">Contact Us</a>
+                <a href="{{ route('contact') }}" class="main" aria-expanded="false">Contact Us</a>
             </li>
         </ul>
     </nav>
