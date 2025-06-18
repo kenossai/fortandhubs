f458351a6ef654845cec4925e0ee2c7d4fdab9aa
Kenneth Ossai <kenossai1@gmail.com>
2025-06-18 13:55:49 +0100

    Update services.blade.php

--- resources/views/pages/services.blade.php
+++ resources/views/pages/services.blade.php
@@ -49,7 +49,6 @@
                                 </div>
                             </div>
                         @endforeach
-
                     </div>
                 </div>
             </div>
