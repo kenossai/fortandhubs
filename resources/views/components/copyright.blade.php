d786b9bf621c138a23189a3c68acfb45ed19d713
Kenneth Ossai <kenossai1@gmail.com>
2025-06-18 13:55:23 +0100

    Update copyright.blade.php

--- resources/views/components/copyright.blade.php
+++ resources/views/components/copyright.blade.php
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col-12">
                 <div class="text-center">
-                    <p>Invena - Copyright
+                    <p>Fort&Hubs - Copyright
                         <script>
                             document.write(
                                 new Date().getFullYear()
