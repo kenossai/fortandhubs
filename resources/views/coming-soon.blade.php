d4791510d56b0c2f52feb4e786063fcd9f3d5bcc
Kenneth Ossai <kenossai1@gmail.com>
2025-06-18 13:55:17 +0100

    Create coming-soon.blade.php

---
+++ resources/views/coming-soon.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="At Fort and Hub Global, we empower organizations to thrive in a rapidly evolving digital landscape by delivering
+                world-class cybersecurity and digital transformation solutions. Our mission is to protect your critical assets, enhance
+                resilience, and drive innovations securely and sustainably.">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/images/fav.png">
+    <title>Coming Soon</title>
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/plugins/fontawesome.css" as="style">
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/plugins/swiper.css" as="style">
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/plugins/metismenu.css" as="style">
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/plugins/magnifying-popup.css" as="style">
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/plugins/odometer.css" as="style">
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/vendor/bootstrap.min.css" as="style">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&family=Red+Hat+Display:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet preload" as="style">
+    <link rel="preload" as="image" href="{{ asset('assets') }}/images/banner/21.webp" />
+    <link rel="stylesheet preload" href="{{ asset('assets') }}/css/style.css" as="style">
+</head>
+
+<body class="account-page-body">
+
+    <!-- rts coming soon area start -->
+    <div class="rts-coming-soon-area rts-section-gap d-flex align-items-center h-100-vh">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="coming-soon-wrapper-main">
+                        <div class="timer-area">
+                            <div class="countdown" id="countdown" data-end-date="2025-06-25T23:59:59">
+                                <h3>🚧Coming Soon</h3>
+                                <div class="timer-section">
+                                    <div class="time-unit">
+                                        <span id="days">0</span>
+                                        <small>Days</small>
+                                    </div>
+                                    <div class="time-unit">
+                                        <span id="hours">0</span>
+                                        <small>Hours</small>
+                                    </div>
+                                    <div class="time-unit">
+                                        <span id="minutes">0</span>
+                                        <small>Minutes</small>
+                                    </div>
+                                    <div class="time-unit">
+                                        <span id="seconds">0</span>
+                                        <small>Seconds</small>
+                                    </div>
+                                </div>
+                            </div>
+                            <p class="para">We're working hard to launch the website. Stay tuned!
+                                Thank you for understanding and patience!</p>
+                            </div>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+    <div id="anywhere-home" class="">
+    </div>
+
+
+    <script defer src="{{ asset('assets') }}/js/plugins/jquery.js"></script>
+
+    <script defer src="{{ asset('assets') }}/js/plugins/odometer.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/jquery-appear.js"></script>
+
+
+    <script defer src="{{ asset('assets') }}/js/plugins/gsap.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/split-text.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/scroll-trigger.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/smooth-scroll.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/metismenu.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/popup.js"></script>
+
+    <script defer src="{{ asset('assets') }}/js/vendor/bootstrap.min.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/swiper.js"></script>
+    <script defer src="{{ asset('assets') }}/js/plugins/contact.form.js"></script>
+
+    <script defer src="{{ asset('assets') }}/js/main.js"></script>
+</body>
+
+
+</html>
