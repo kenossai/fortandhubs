b7369555511c08645111a1ac2fa362b05eea628e
Kenneth Ossai <kenossai1@gmail.com>
2025-06-23 16:03:17 +0100

    Update footer.blade.php

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,26 +1,105 @@
-<footer class="footer-area">
-          <div class="container large">
-            <div class="copyright-area-inner">
-              <div class="copyright-text">
-                <p class="text">© 2024-25 <a href="https://themeforest.net/user/crowdytheme"
-                    target="_blank">crowdyTheme</a> Agency</p>
-              </div>
-              <ul class="footer-nav-list">
-                <li><a href="web-agency.html#">Agency</a></li>
-                <li><a href="web-agency.html#">Career</a></li>
-                <li><a href="web-agency.html#">Privacy</a></li>
-                <li><a href="web-agency.html#">Terms & condition</a></li>
-              </ul>
-              <ul class="social-links">
-                <li><a href="web-agency.html#"><i class="fa-brands fa-facebook-f"></i></a></li>
-                <li><a href="web-agency.html#"><i class="fa-brands fa-twitter"></i></a></li>
-                <li><a href="web-agency.html#"><i class="fa-brands fa-instagram"></i></a></li>
-                <li><a href="web-agency.html#"><i class="fa-brands fa-linkedin"></i></a></li>
-              </ul>
-
+ <!-- contact area start  -->
+<section class="contact-area">
+    <div class="container">
+        <div class="contact-area-inner section-spacing">
+        <div class="shape-1">
+            <img src="assets/imgs/shape/img-s-70.webp" alt="shape">
+        </div>
+        <div class="section-content">
+            <div class="section-title-wrapper">
+            <div class="title-wrapper">
+                <h2 class="section-title has_text_move_anim">Let’s start your
+                experience with Arolax</h2>
             </div>
-          </div>
-        </footer>
+            </div>
+            <div class="btn-wrapper has_fade_anim">
+            <a href="contact-bold.html" class="wc-btn wc-btn-underline btn-text-flip"> <span
+                data-text="Let’s get in touch">Let’s get in touch</span> <img
+                src="assets/imgs/icon/arrow-right-half-light.webp" alt="icon image"></a>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+          <!-- contact area end  -->
+          <!-- cta area start  -->
+<section class="cta-area">
+    <div class="container">
+        <div class="cta-area-inner section-spacing">
+        <div class="section-header">
+            <div class="section-title-wrapper">
+            <div class="subtitle-wrapper">
+                <span class="section-subtitle has_fade_anim">🖐️ Hello
+                <img class="show-light" src="assets/imgs/shape/img-s-6.webp" alt="image">
+                <img class="show-dark" src="assets/imgs/shape/img-s-6-light.webp" alt="image">
+                </span>
+            </div>
+            <div class="title-wrapper">
+                <h2 class="section-title has_fade_anim">Let’s Talk
+                with us</h2>
+            </div>
+            </div>
+            <div class="btn-wrapper has_fade_anim">
+            <div class="footer-widget-wrapper newsletter">
+                <style>
+                .subscribe-form {
+                    display: block;
+                    align-items: center;
+                    unicode-bidi: isolate
+                }
+                .subscribe-form .input-field {
+                    display: flex;
+                    gap: 10px;
+                    border-bottom: 1px solid var(--white);
+                    padding-bottom: 25px;
+                }
+                .subscribe-form .input-field input {
+                    width: 100%;
+                    background: none;
+                    border: none;
+                    outline: none;
+                    color: var(--white);
+                    font-size: 24px;
+                    font-weight: 300;
+                    padding: 10px 0;
+                }
+                </style>
+            <form action="#" class="subscribe-form">
+            <div class="input-field">
+                <input type="email" placeholder="Enter your email">
+                <button type="submit" class="subscribe-btn"><img src="assets/imgs/icon/arrow-light.webp" alt="icon"></button>
+            </div>
+            </form>
+        </div>
+            </div>
+        </div>
+        </div>
+    </div>
+</section>
+<!-- cta area end  -->
+<footer class="footer-area">
+    <div class="container large">
+    <div class="copyright-area-inner">
+        <div class="copyright-text">
+        <p class="text">© 2024-25 <a href="https://themeforest.net/user/crowdytheme"
+            target="_blank">crowdyTheme</a> Agency</p>
+        </div>
+        <ul class="footer-nav-list">
+        <li><a href="web-agency.html#">Agency</a></li>
+        <li><a href="web-agency.html#">Career</a></li>
+        <li><a href="web-agency.html#">Privacy</a></li>
+        <li><a href="web-agency.html#">Terms & condition</a></li>
+        </ul>
+        <ul class="social-links">
+        <li><a href="web-agency.html#"><i class="fa-brands fa-facebook-f"></i></a></li>
+        <li><a href="web-agency.html#"><i class="fa-brands fa-twitter"></i></a></li>
+        <li><a href="web-agency.html#"><i class="fa-brands fa-instagram"></i></a></li>
+        <li><a href="web-agency.html#"><i class="fa-brands fa-linkedin"></i></a></li>
+        </ul>
+
+    </div>
+    </div>
+</footer>
 
 
 
