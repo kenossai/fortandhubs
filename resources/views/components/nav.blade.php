3b9229d773b0c969391257cdb2f0f6a0da8cc6d1
Kenneth Ossai <kenossai1@gmail.com>
2025-06-23 16:03:19 +0100

    Update nav.blade.php

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -11,77 +11,16 @@
           <nav class="main-menu">
             <ul>
               <li class="menu-item-has-children">
-                <a href="web-agency.html#">Demos</a>
-                <ul class="dp-menu col-2">
-                  <li><a href="branding-agency.html">Branding Agency</a></li>
-                  <li><a href="web-agency.html">Web Agency</a></li>
-                  <li><a href="seo-agency.html">SEO Agency</a></li>
-                  <li><a href="design-studio.html">Design Agency</a></li>
-                  <li><a href="video-production.html">Video Agency</a></li>
-                  <li><a href="ai-agency.html">AI Agency</a></li>
-                  <li><a href="creative-agency-classic.html">Creative Agency Classic</a></li>
-                  <li><a href="marketing-agency.html">Marketing Agency</a></li>
-                  <li><a href="corporate-agency.html">Corporate Agency</a></li>
-                  <li><a href="startup-agency.html">Startup Agency</a></li>
-                  <li><a href="modern-agency.html">Modern Agency</a></li>
-                  <li><a href="photography-studio.html">Photography Agency</a></li>
-                  <li><a href="creative-agency.html">Creative Agency</a></li>
-                  <li><a href="digital-agency.html">Digital Agency</a></li>
-                </ul>
-              </li>
-              <li><a href="about.html">about</a></li>
-              <li class="menu-item-has-children">
-                <a href="web-agency.html#">portfolio</a>
-                <ul class="dp-menu">
-                  <li><a href="portfolio-carousel.html">Portfolio Carousel</a></li>
-                  <li><a href="portfolio-material.html">Portfolio Material</a></li>
-                  <li><a href="portfolio-slicer.html">Portfolio Slicer</a></li>
-                  <li><a href="portfolio-spring.html">Portfolio Spring</a></li>
-                </ul>
-              </li>
-              <li class="menu-item-has-children">
-                <a href="web-agency.html#">Pages</a>
-                <ul class="dp-menu">
-                  <li class="menu-item-has-children">
-                    <a href="web-agency.html#">service pages</a>
-                    <ul>
-                      <li><a href="services.html">services</a></li>
-                      <li><a href="service-details.html">service details</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="web-agency.html#">project pages</a>
-                    <ul>
-                      <li><a href="works.html">project</a></li>
-                      <li><a href="work-details.html">project details</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="web-agency.html#">team pages</a>
-                    <ul>
-                      <li><a href="team.html">team</a></li>
-                      <li><a href="team-details.html">team details</a></li>
-                    </ul>
-                  </li>
-                  <li class="menu-item-has-children">
-                    <a href="web-agency.html#">career pages</a>
-                    <ul>
-                      <li><a href="career.html">career</a></li>
-                      <li><a href="career-details.html">career details</a></li>
-                    </ul>
-                  </li>
-                  <li><a href="faq.html">faq</a></li>
-                  <li><a href="404.html">404 page</a></li>
-                </ul>
-              </li>
-              <li>
-                <a href="web-agency.html#">blog</a>
-                <ul class="dp-menu">
-                  <li><a href="blog.html">blog</a></li>
-                  <li><a href="blog-details.html">blog details</a></li>
-                </ul>
-              </li>
-              <li><a href="contact.html">Contact</a></li>
+                <a href="/">Home</a>
+            </li>
+            <li><a href="about-us">about</a></li>
+            <li class="menu-item-has-children">
+                <a href="service">Service</a>
+            </li>
+            <li class="menu-item-has-children">
+                <a href="projects">Projects</a>
+            </li>
+              <li><a href="contact-us">Contact</a></li>
             </ul>
           </nav>
         </div>
