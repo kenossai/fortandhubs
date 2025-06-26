0faaa911abc321c83833d901a562add76370c28f
Kenneth Ossai <kenossai1@gmail.com>
2025-06-26 23:43:19 +0100

    Update clients.blade.php

--- resources/views/components/clients.blade.php
+++ resources/views/components/clients.blade.php
@@ -2,36 +2,13 @@
                     <div class="dgm-brand-wrapper">
                         <div class="swiper-container dgm-brand-active">
                             <div class="swiper-wrapper">
-                                <div class="swiper-slide">
-                                    <div class="dgm-brand-item">
-                                        <img src="assets/img/home-03/brand/brand-1.png" alt="">
+                                @foreach ($clients as $client)
+                                    <div class="swiper-slide">
+                                        <div class="dgm-brand-item">
+                                            <img src="{{ asset('storage/' . $client->image_path) }}" alt="{{ $client->name }}" width="100">
+                                        </div>
                                     </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="dgm-brand-item">
-                                        <img src="assets/img/home-03/brand/brand-2.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="dgm-brand-item">
-                                        <img src="assets/img/home-03/brand/brand-3.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="dgm-brand-item">
-                                        <img src="assets/img/home-03/brand/brand-4.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="dgm-brand-item">
-                                        <img src="assets/img/home-03/brand/brand-5.png" alt="">
-                                    </div>
-                                </div>
-                                <div class="swiper-slide">
-                                    <div class="dgm-brand-item">
-                                        <img src="assets/img/home-03/brand/brand-6.png" alt="">
-                                    </div>
-                                </div>
+                                @endforeach
                             </div>
                         </div>
                     </div>
